05f854e9d67212e4d8ce63cc23c7dab6dca70445
karthikselvaam <karthikselvaam@gmail.com>
2021-04-12 14:00:27 +0530

    binary search

---
+++ Searching/binarysearch.py
@@ -0,0 +1,19 @@
+arr = [1,2,3,5,6,7,8,9,11]
+target = 4
+
+def binarysearch(arr, target):
+    low = 0
+    high = len(arr)-1
+    
+    while (low <= high):
+        mid = low + high // 2  
+        if arr[mid] < target:
+            low = mid + 1 
+        elif arr[mid] > target :
+            high = mid - 1
+        else:
+            return arr[mid]
+
+
+result = binarysearch(arr,8)
+print(result)
